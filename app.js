457b338bca23a0c5bed3df67c72764d82233e08c
Paul Bennings <>
2019-07-23 11:37:53 -0400

    second commit after starting server, still works...used 'test' for console.log

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const nodemailer = require('nodemailer')
 
 const app = express();
 
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Test');
 });
 
 app.listen(3000, () => console.log('Server started...'));
