ec6500ff2f05fca4d10d9b428ba228078cb92981
Paul Bennings <>
2019-07-23 11:55:43 -0400

    added public folder

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-const nodemailer = require('nodemailer')
+const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -9,10 +10,15 @@ const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 // Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Test');
+  res.send('hello');
 });
 
 app.listen(3000, () => console.log('Server started...'));
